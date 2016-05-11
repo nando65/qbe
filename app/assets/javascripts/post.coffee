891bcb3af2ed9fad64b256187acb68f99bd8d986
Nicolas Iragorri <nid@eficiatech.com>
2016-05-11 02:58:25 -0430

    cambios like, comment y share en infinite scroll

--- app/assets/javascripts/post.coffee
+++ app/assets/javascripts/post.coffee
@@ -1,8 +1,9 @@
 jQuery ->
    if $('.pagination').length
+     requested_links = []
      $(window).scroll ->
         url = decodeURIComponent( $('.last-loaded-page:last').val() )
-        if url && $(window).scrollTop() > $(document).height() - $(window).height() - 20
+        if url && $(window).scrollTop() > $(document).height() - $(window).height() - 20 && $.inArray(url, requested_links) < 0
+          requested_links.push(url)
           $('.pagination').text("Fetching more results...")
           $.getScript(url)
-      $(window).scroll()
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
     @page = params[:page]
     @page = 1 if params[:page].nil?
     if current_user.feed(0,10).count == 0
-    @last_users = User.first(20).reverse
+    	@last_users = User.first(20).reverse
     else
-    @posts = current_user.feed(@page,10).order('created_at DESC')
+    	@posts = current_user.feed(@page,10).order('created_at DESC')
     end
     @comment = Comment.new
   end
@@ -26,14 +26,38 @@ class HomeController < ApplicationController
     redirect_to controller: :home, action: :index
   end
 
+ 	def create_like_ajax
+    Like.like(params[:post_id], params[:current_user_id])
+		render partial: '/home/destroy_like', locals: { t: Post.find(params[:post_id]), new_class: ' like-link-new', destroy_action: 'destroy_like_ajax' } 
+  end
+
+  def destroy_like_ajax
+    Like.destroy_all(user_id: params[:current_user_id], post_id: params[:post_id])
+		render partial: '/home/create_like', locals: { t: Post.find(params[:post_id]), new_class: ' like-link-new', create_action: 'create_like_ajax' } 
+  end
+
+
   def create_like_comment
     Like.like_comment(params[:comment_id], params[:current_user_id])
     redirect_to :controller => 'home', :action => 'index'
   end
 
+  def create_like_comment_ajax
+    Like.like_comment(params[:comment_id], params[:current_user_id])
+		render partial: '/home/destroy_like_comment', locals: { d: Comment.find(params[:comment_id]), new_like: ' like-link-new', destroy_action: 'destroy_like_comment_ajax' } 
+  end
+
+
   def destroy_like_comment
     Like.destroy_all(user_id: params[:current_user_id], comment_id: params[:comment_id])
     redirect_to controller: :home, action: :index
   end
+
+  def destroy_like_comment_ajax
+    Like.destroy_all(user_id: params[:current_user_id], comment_id: params[:comment_id])
+		render partial: '/home/create_like_comment', locals: { d: Comment.find(params[:comment_id]), new_like: ' like-link-new', create_action: 'create_like_comment_ajax' } 
+		#render partial: '/home/create_like_comment'
+  end
+
 
 end
